6c7b21b8608adf29f1ab16d732bd450d00cd6a43
teodorescu-a-vlad <teodorescu.a.vlad@gmail.com>
2023-01-24 18:05:04 +0200

    Initial commit

---
+++ src/main/java/com/LFC/VladT/Exercitiul1.java
@@ -0,0 +1,11 @@
+package com.LFC.VladT;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scaner=new Scanner(System.in);
+        System.out.println("Hello world!");
+        String read = scaner.nextLine();
+    }
+}
